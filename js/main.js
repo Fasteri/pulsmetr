dd7a4d2f1f865041c3090433e0584e629d318274
FastBro <draykin1701@gmail.com>
2022-07-20 23:23:02 +0300

    2.21

--- js/main.js
+++ js/main.js
@@ -94,5 +94,27 @@ $(document).ready(function () {
     autoclear: false
   });
 
+  $('form').submit(function (e) { 
+    e.preventDefault();
+    
+
+    if (!$(this).valid()){
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize() 
+    }).done(function(){
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn();
+
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 
 });
