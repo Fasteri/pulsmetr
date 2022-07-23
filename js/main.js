4fbcbf948bccb925cf332f3a68b87e48c7849858
FastBro <draykin1701@gmail.com>
2022-07-23 19:07:55 +0300

    2.3

--- js/main.js
+++ js/main.js
@@ -93,6 +93,30 @@ $(document).ready(function () {
   $('input[name=phone]').mask("+7 (999) 999-99-99", {
     autoclear: false
   });
+// --------------------------------------------------------
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 1600){
+    $('.pageup').fadeIn();
+  } else{
+    $('.pageup').fadeOut();
+  }
+});
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+
+
+
+
+
+
+
+
 // ---------------------------------------------------
 
 
